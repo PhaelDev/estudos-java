fbea514b17eb2fd8331367704b1419c96846ed51
Raphael Souza <raphaelsouza.dev@gmail.com>
2026-03-19 19:26:44 -0400

    aula inicial de tipos primitivos em java

---
+++ maratona-java/src/introducao/TiposPrimitivos.java
@@ -0,0 +1,11 @@
+package introducao;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        //int, double, float, char, byte, short, long, boolean
+
+        int idade=10;
+
+        System.out.println("A idade é: "+idade+" anos!");
+    }
+}
