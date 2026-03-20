4de15c7ba73c8803ac470842111db7453e943c7e
Raphael Souza <raphaelsouza.dev@gmail.com>
2026-03-20 13:25:20 -0400

    Meu primeiro exercício java com tipos primitivos

--- maratona-java/src/introducao/TiposPrimitivos.java
+++ maratona-java/src/introducao/TiposPrimitivos.java
@@ -5,6 +5,14 @@ public class TiposPrimitivos {
         //int, double, float, char, byte, short, long, boolean
 
         int idade=10;
+        long numeroGrande = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'R';
 
         System.out.println("A idade é: "+idade+" anos!");
     }
---
+++ maratona-java/src/introducao/TiposPrimitivosExercicios.java
@@ -0,0 +1,29 @@
+package introducao;
+
+/*
+* Prática
+*
+* Crie variaveis para os campos descritos abaixo entre <> e imprima a seguinte mensagem:
+*
+* Eu <nome>, morando na cidade <cidade>,
+* confirmo que recebi o salario de <salario>, na data <data>.
+*
+* */
+
+public class TiposPrimitivosExercicios {
+    public static void main(String[] args) {
+
+        String nome = "Raphael";
+        String cidade = "Manaus";
+        double salario = 2500D;
+        String data = "20/03/2026";
+
+        System.out.println("Eu "+nome+", morando na cidade "+cidade+",\n" +
+                "confirmo que recebi o salario de R$: "+salario+", na data "+data+".");
+
+    }
+
+
+
+
+}
